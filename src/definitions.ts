bf752b3ec1d30eb3e17cd8692e855325eb69ba77
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2024-07-10 17:03:21 +0200

    feat: modify interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,67 @@
+export enum SigningErrorType {
+  /**
+   * The key is null or empty.
+   */
+  missingKey = 'missingKey',
+
+  /**
+   * An error occurred while interacting with Keystore
+   */
+  keystoreError = 'keystoreError',
+
+  /**
+   * An error occurred while generating the key
+   */
+  keyGenerationError = 'keyGenerationError',
+
+  /**
+   * An error occurred while generating the key
+   */
+  capacitorError = 'capacitorError',
+
+  /**
+   * `get()` found the data, but it is corrupted.
+   */
+  invalidData = 'invalidData',
+
+  /**
+   * A system-level error occurred when getting/setting data from/to the store.
+   */
+  osError = 'osError',
+
+  /**
+   * An unclassified system-level error occurred.
+   */
+  unknownError = 'unknownError',
+
+  /**
+   * Secure lock screen must be enabled to create keys requiring user authentication
+   * */
+  secureLockScreenDisabled = 'secureLockScreenDisabled',
+
+  /**
+   * User must be authenticated to access the secure storage
+   * */
+  userNotAuthenticated = 'userNotAuthenticated',
+}
+
+/**
+ * If one of the storage functions throws, it will throw a SigningError which
+ * will have a .code property that can be tested against SigningErrorType,
+ * and a .message property will have a message suitable for debugging purposes.
+ *
+ * @modified 5.0.0
+ */
+export class SigningError extends Error {
+  code: SigningErrorType;
+
+  constructor(message: string, code: SigningErrorType) {
+    super(message);
+    this.name = this.constructor.name;
+    this.code = code;
+  }
+}
+
 export interface SecureSigningPlugin {
   doesKeyPairExist(options: {
     prefixedKey: string;
